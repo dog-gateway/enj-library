40e29f5dda788c59cd25e6a6d9d0d5e8facef91f
andreabiasi <biasiandrea04@gmail.com>
2013-12-16 13:57:56 +0100

    updating of HiPriorityThread

--- src/it/polito/elite/enocean/protocol/serial/v3/network/serialcomunication/HighPriorityThread.java
+++ src/it/polito/elite/enocean/protocol/serial/v3/network/serialcomunication/HighPriorityThread.java
@@ -3,6 +3,7 @@ package it.polito.elite.enocean.protocol.serial.v3.network.serialcomunication;
 import java.util.concurrent.Semaphore;
 
 import it.polito.elite.enocean.protocol.serial.v3.network.packet.Packet;
+import it.polito.elite.enocean.protocol.serial.v3.network.packet.response.Response;
 
 /**
  * This class look if there are high priority message received (ones which needs response), process them and put eventually response into the high priority message in transmission thread
@@ -21,31 +22,31 @@ public class HighPriorityThread extends Thread {
 		this.mutexHpTxQueue = mutexHpTxQueue;
 		this.flagResponse = flagResponse;
 	}
-	
+
 	// Coda di messaggi dal thread di ricezione
 	DataQueue highPriorityRxQueue;
-	
+
 	// Coda di messaggi ad alta prioritˆ dal thread di scrittura
 	DataQueue highPriorityTxQueue;
-	
+
 	// Semaforo per l'accesso alla coda del thread di ricezione
 	Semaphore mutexHpRxQueue;
-	
+
 	// Semaforo per l'accesso alla coda del thread di trasmissione
 	Semaphore mutexHpTxQueue;
-	
+
 	// Pacchetto ESP3
 	Packet pkt;
-	
+
 	boolean flagResponse;
-	
+
 	public void run(){
 		boolean canRun = true;
 		while(canRun){
 			// Prende i pacchetti dalla coda RX e li mette nella coda in TX
 			if (!this.highPriorityRxQueue.isEmpty()){
 				//this.highPriorityTxQueue.add(this.highPriorityRxQueue.poll());
-				
+
 				synchronized(highPriorityRxQueue){
 					while (!this.mutexHpRxQueue.tryAcquire()) {
 						try {
@@ -55,15 +56,55 @@ public class HighPriorityThread extends Thread {
 							return;
 						}
 					}
-					
+
 					// Estae l'elemento dalla coda
 					pkt = this.highPriorityRxQueue.poll();
-					
+
 					// Setto risposta attesa
 					this.flagResponse = true;
-					
+
 					// Libero il thread di lettura che condivide la risorsa
 					this.highPriorityRxQueue.notify();
+				}
+
+				/*
+				 * Processamento risposta
+				 */
+				
+				Response response = null; //Poi si pu˜ pure togliere questa inizializzazione
+
+				switch (pkt.getData()[0]) {
+				case 0x01:
+					//Setto solo il campo Return Code chiamando il costruttore
+
+					// RET_OK
+					response = new Response((byte)0X00);
+
+					// RET_ERROR
+					response = new Response((byte)0X01);
+
+					// RET_ERROR
+					response = new Response((byte)0X02);
+
+					// RET_WRONG_PARAM
+					response = new Response((byte)0X03);
+
+				case 0x02: 
+
+				case 0x03: 
+					//Setto solo il campo Return Code chiamando il costruttore
+
+					// RET_OK
+					response = new Response((byte)0X00);
+
+					// RET_ERROR
+					response = new Response((byte)0X01);
+
+					// RET_ERROR
+					response = new Response((byte)0X02);
+
+					// RET_WRONG_PARAM
+					response = new Response((byte)0X03);
 				}
 				
 				synchronized(highPriorityTxQueue){
@@ -75,13 +116,13 @@ public class HighPriorityThread extends Thread {
 							return;
 						}
 					}
-					
-					// Aggiungi l'elemento estratto dal thread di lettura e lo aggiunge al thread di scrittura
-					this.highPriorityTxQueue.add(pkt);
-					
+
+					// Aggiungi la risposta da inviare in high priority TX queue					
+					this.highPriorityTxQueue.add(response); // NB in questo caso ho solo messo la risposta Ret_OK
+
 					// Libera risposta attesa
 					this.flagResponse = false;
-					
+
 					// Libera il thread di scrittura che condivide la risorsa
 					this.highPriorityTxQueue.notify();
 				}	
